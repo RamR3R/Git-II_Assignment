47967d5791b858fcf5583669dd5a0eb6f8145c0c
Tarun Ergurala <112754119+tarune7030@users.noreply.github.com>
2023-01-03 20:19:04 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -3,7 +3,7 @@ var empty = "";
 for (i = name.length - 1; i >= 0; i--) {
   empty = empty + name[i];
 }
-if ((name = empty)) {
+if (name == empty) {
   console.log("Yes");
 } else {
   console.log("No");
